931e1c6243ac76a0e17cffa2160fbf5be6f97703
Anthony Rinaldi <AnthonyLeeRinaldi@gmail.com>
2012-11-05 22:46:10 -0500

    MaxHeap

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,16 +1,100 @@
 
-public class MaxHeap
+// -------------------------------------------------------------------------
+/**
+ *  This is a MaxHeap; a heap with the largest value at the top.
+ *
+ *  @author Anthony Rinaldi
+ *  @author Ryan Merkel
+ *  @version November 5, 2012
+ * @param <E> Generic
+ */
+public class MaxHeap<E extends Comparable<? super E>>
 {
+    /**
+     * Max-Size of the Heap
+     */
+    private int maxn;
+    /**
+     * Length of the MaxHeap
+     */
     int len;
+    /**
+     * The HeapArray
+     */
+    E array[];
 
     // ----------------------------------------------------------
     /**
      * Create a new MaxHeap.
+     * @param inputarray Unsorted input values
+     * @param number
+     * @param max
      */
-    public MaxHeap()
+    public MaxHeap(E[] inputarray, int number, int max)
     {
         // Public Constructor
-        len = 0;
+        len = number;
+        maxn = max;
+        // Set array to be inputarray, then heapify.
+        array = inputarray;
+        heapify();
+    }
+
+    /**
+     * This helper method heapifies the array.
+     */
+    private void heapify()
+    {
+        for (int i = len / 2 - 1; i >= 0; i--)
+            siftdown(i);
+    }
+
+
+    /** Put element in its correct place */
+    private void siftdown(int pos)
+    {
+        assert (pos >= 0) && (pos < len) : "Illegal heap position";
+        while (!isLeaf(pos))
+        {
+            int j = leftchild(pos);
+            if ((j<(len - 1)) && (array[j].compareTo(array[j+1]) < 0))
+                j++; // j is now index of child with greater value
+            if (array[pos].compareTo(array[j]) >= 0) return;
+                swap(array, pos, j);
+        pos = j; // Move down
+        }
+    }
+
+    private void swap(E[] array2, int pos, int j)
+    {
+        E temp = array2[pos];
+        array2[pos]= array2[j];
+        array2[j] = temp;
+    }
+
+    /** @return Position for left child of pos */
+    public int leftchild(int pos)
+    {
+        assert pos < len/2 : "Position has no left child";
+        return 2*pos + 1;
+    }
+    /** @return Position for right child of pos */
+    public int rightchild(int pos)
+    {
+        assert pos < (len-1)/2 : "Position has no right child";
+        return 2*pos + 2;
+    }
+    /** @return Position for parent */
+    public int parent(int pos)
+    {
+        assert pos > 0 : "Position has no parent";
+        return (pos-1)/2;
+    }
+
+    private boolean isLeaf(int pos)
+    {
+        // TODO Auto-generated method stub
+        return false;
     }
 
     // ----------------------------------------------------------
