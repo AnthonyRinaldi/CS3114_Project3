57b6547a1cdda5c52c8f900bf0d89eb21c5f9061
Anthony  Rinaldi <AnthonyLeeRinaldi@gmail.com>
2012-11-05 21:31:21 -0500

    Heapsort
    Continue work on developing Heapsorter, including starting MaxHeap
    class.

--- src/Heapsorter.java
+++ src/Heapsorter.java
@@ -1,13 +1,49 @@
 
 // -------------------------------------------------------------------------
 /**
- *  This program is an implimentatoin of Heapsort.
+ *  This program is an implementation of Heapsort.
  *
  *  @author Anthony Rinaldi
  *  @author Ryan Merkel
- *  @version Nov 5, 2012
+ *  @version November 5, 2012
  */
 public class Heapsorter
 {
-    // Insert stuff here...
+
+    /**
+     * The time it took for the last sort in milliseconds
+     */
+    long time = -1;
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @param input
+     */
+    void heapsorter(int input)
+    {
+        // Get the initial time
+        long startTime = System.currentTimeMillis();
+/*
+        MaxHeap H = new MaxHeap(input, input.length(), input.length());
+        for (int i=0; i < input.length(); i++)  // Sort
+          H.removemax(); // Removemax places max at end of heap
+*/
+        // Get the end time
+        long endTime = System.currentTimeMillis();
+        // Calculate the total time
+        time = endTime - startTime;
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Returns the last sort's calculation time in milliseconds. Negative
+     * implies no sort time.
+     * @return The sort time
+     */
+    public long lastSortTime()
+    {
+        return time;
+    }
 }
---
+++ src/MaxHeap.java
@@ -0,0 +1,36 @@
+
+public class MaxHeap
+{
+    int len;
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new MaxHeap.
+     */
+    public MaxHeap()
+    {
+        // Public Constructor
+        len = 0;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Swaps the maximum value in the heap with the end value.
+     */
+    public void removemax()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Return the length of the MaxHeap
+     * @return length of the MaxHeap
+     */
+    public int length()
+    {
+        return len;
+    }
+
+}
